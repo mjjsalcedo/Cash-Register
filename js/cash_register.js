811dc83a8aa43c08d6f127577d2055cc37bc9ee2
Mariel Salcedo <Mariel Salcedo>
2017-06-17 21:16:39 -1000

    it can iterate one operator at a time

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,164 +1,199 @@
 /*jshint esversion: 6 */
+(function(window){
+
+
 const calc = window.calculator;
 var image = document.getElementById("display");
-var total = 0;
 var balance = 0;
-var operation = null;
-var storedNumber;
 
+let operation = null;
+let storedNumber = "";
 
-function clearData(){
-    return image.innerHTML = "";
+  function clearScreen(){
+     reset = image.innerHTML = "";
+     return reset;
+  }
+  function storeValue( nextValue ){
+    storedNumber = nextValue;
+
+  }
+  function clearValue(){
+    storedNumber = "";
   }
 
-window.cashRegister = (function(){
+  function showDisplay(){
+    image.innerHTML = storedNumber;
+}
+
+const restart = document.getElementById("clear");
+      restart.addEventListener("click", function(event){
+        image.innerHTML = "";
+      });
+const num1 = document.getElementById("button1");
+      num1.addEventListener("click", function(event){
+        storeValue("1");
+        showDisplay();
+      });
+const num2 = document.getElementById("button2");
+      num2.addEventListener("click", function(event){
+        storeValue("2");
+        showDisplay();
+      });
+const num3 = document.getElementById("button3");
+      num3.addEventListener("click", function(event){
+        storeValue("3");
+        showDisplay();
+      });
+const num4 = document.getElementById("button4");
+      num4.addEventListener("click", function(event){
+        storeValue("4");
+        showDisplay();
+      });
+const num5 = document.getElementById("button5");
+      num5.addEventListener("click", function(event){
+        storeValue("5");
+        showDisplay();
+      });
+const num6 = document.getElementById("button6");
+      num6.addEventListener("click", function(event){
+        storeValue("6");
+        showDisplay();
+      });
+const num7 = document.getElementById("button7");
+      num7.addEventListener("click", function(event){
+        storeValue("7");
+        showDisplay();
+      });
+const num8 = document.getElementById("button8");
+      num8.addEventListener("click", function(event){
+        storeValue("8");
+        showDisplay();
+      });
+const num9 = document.getElementById("button9");
+      num9.addEventListener("click", function(event){
+        storeValue("9");
+        showDisplay();
+      });
+const num0 = document.getElementById("button0");
+      num0.addEventListener("click", function(event){
+        storeValue("0");
+        showDisplay();
+      });
+const num00 = document.getElementById("button00");
+      num00.addEventListener("click", function(event){
+        storeValue("00");
+        showDisplay();
+      });
+const num = document.getElementById("button");
+      num.addEventListener("click", function(event){
+        if( storedNumber.indexOf(".") < 0){
+          storeValue(".");
+        showDisplay();
+        }
+      });
+
+var conquer = document.getElementById("divide");
+    conquer.addEventListener('click',function(event){
+    if(operation!== null){
+
+      if( storedNumber !== "" ){
+        meow(operation);
+      }
+
+    } else {
+
+    operation = "divide";
+    calc.load(parseFloat(storedNumber));
+    clearValue();
+  }
+  });
+
+var reproduce = document.getElementById("multiply");
+    reproduce.addEventListener('click',function(event){
+    if(operation!== null){
+
+      if( storedNumber !== "" ){
+        meow(operation);
+      }
+
+    } else {
+
+    operation = "multiply";
+    calc.load(parseFloat(storedNumber));
+    clearValue();
+  }
+  });
+
+var negative = document.getElementById("subtract");
+    negative.addEventListener('click',function(event){
+
+    if(operation!== null){
+
+      if( storedNumber !== "" ){
+        meow(operation);
+      }
+
+    } else {
+
+    operation = "subtract";
+    calc.load(parseFloat(storedNumber));
+    clearValue();
+  }
+  });
+
+var positive = document.getElementById("add");
+    positive.addEventListener('click',function(event){
+
+    if(operation!== null){
+
+      if( storedNumber !== "" ){
+        meow(operation);
+      }
+
+    } else {
+
+    calc.load(parseFloat(storedNumber));
+    clearValue();
+  }
+    operation = "add";
+  });
+
+var transform = document.getElementById("equal");
+    transform.addEventListener('click',function(event){
+    clearScreen();
+    meow(operation);
+
+
+  });
 
 function meow(operation){
 
     switch(operation){
 
       case 'add':
-         balance = calc.add(storedNumber);
+         calc.add(parseFloat(storedNumber));
         break;
 
-
       case 'subtract':
-        balance = calc.subtract(storedNumber);
+        calc.subtract(parseFloat(storedNumber));
         break;
 
       case 'multiply':
-        balance = calc.multiply(storedNumber);
+        calc.multiply(parseFloat(storedNumber));
         break;
 
       case 'divide':
-        balance = calc.divide(storedNumber);
+        calc.add(parseFloat(storedNumber));
+        break;
+      default:
+        throw new Error("shit doesn't work yo -_-");
       }
+      console.log("calc getTotal",calc.getTotal());
+      storeValue(calc.getTotal());
 
-       return balance;
+      showDisplay();
 
+      operation = null;
 
 }
-
-  function loading(x) {
-    total = calc.load(parseFloat(x));
-    console.log(total);
-  }
-
-  function getTotal() {
-    return calc.getTotal();
-  }
-
-  function getBalance() {
-    return balance;
-  }
-
-  function depositCash(x) {
-    if(x > 0) {
-      return balance += parseFloat(x);
-    }
-  }
-
-  function withdrawCash(x) {
-    return balance -= parseFloat(x);
-  }
-
-  return {
-    getTotal : getTotal,
-    loading : loading,
-    meow : meow,
-    getBalance : getBalance,
-    depositCash : depositCash,
-    withdrawCash : withdrawCash
-
-  };
-})();
-
-console.log(total);
-
-const restart = document.getElementById("clear");
-      restart.addEventListener("click", function(){
-        image.innerHTML = "";
-      });
-const num1 = document.getElementById("button1");
-      num1.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "1";
-      });
-const num2 = document.getElementById("button2");
-      num2.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "2";
-      });
-const num3 = document.getElementById("button3");
-      num3.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "3";
-      });
-const num4 = document.getElementById("button4");
-      num4.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "4";
-      });
-const num5 = document.getElementById("button5");
-      num5.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "5";
-      });
-const num6 = document.getElementById("button6");
-      num6.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "6";
-      });
-const num7 = document.getElementById("button7");
-      num7.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "7";
-      });
-const num8 = document.getElementById("button8");
-      num8.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "8";
-      });
-const num9 = document.getElementById("button9");
-      num9.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "9";
-      });
-const num0 = document.getElementById("button0");
-      num0.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "0";
-      });
-const num00 = document.getElementById("button00");
-      num00.addEventListener("click", function(){
-        storedNumber = image.innerHTML += "00";
-      });
-const num = document.getElementById("button");
-      num.addEventListener("click", function(){
-        storedNumber = image.innerHTML += ".";
-      });
-
-var conquer = document.getElementById("divide");
-    conquer.addEventListener('click',function(){
-    operation = "divide";
-    cashRegister.loading(storedNumber);
-    clearData();
-  });
-
-var reproduce = document.getElementById("multiply");
-    reproduce.addEventListener('click',function(){
-    operation = "multiply";
-    cashRegister.loading(storedNumber);
-    clearData();
-  });
-
-var negative = document.getElementById("subtract");
-    negative.addEventListener('click',function(){
-    operation = "subtract";
-    cashRegister.loading(storedNumber);
-    clearData();
-  });
-
-var positive = document.getElementById("add");
-    positive.addEventListener('click',function(){
-    operation = "add";
-    cashRegister.loading(storedNumber);
-    clearData();
-  });
-
-var transform = document.getElementById("equal");
-    transform.addEventListener('click',function(){
-    cashRegister.meow(operation);
-    console.log("equal:", cashRegister.meow(operation));
-
-  });
+}(window));
